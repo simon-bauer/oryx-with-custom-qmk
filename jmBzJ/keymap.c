4a5d769bbb8dec3b6c56a8a4f0ecc9826260b0da
simon-bauer <bauer.simon@gmx.de>
2026-04-16 12:46:00 +0200

    Increase alt tab timer threshold to 1000ms

--- jmBzJ/keymap.c
+++ jmBzJ/keymap.c
@@ -776,7 +776,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 void matrix_scan_user(void) { // alt tab timer.
   if (is_alt_tab_active) {
-    if (timer_elapsed(alt_tab_timer) > 800) {
+    if (timer_elapsed(alt_tab_timer) > 1000) {
       unregister_code(KC_LALT);
       wait_ms(2);
       is_alt_tab_active = false;
